06cb44a5f91c4a2a6b2565dcf9fb654fc585464a
vtAndreola <dev.andreola@gmail.com>
2024-08-01 10:16:43 -0300

    fix: project page title mobile layout

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -29,35 +29,41 @@ export default function Projects() {
     <div>
       <main className="border-b bg-muted">
         <div className="container mx-auto max-w-6xl py-6 md:py-12">
-          <div className="mb-8 flex items-center justify-between md:mb-10 lg:mb-12">
-            <h1 className="text-2xl font-bold tracking-tight text-foreground sm:text-3xl md:text-4xl">
+          <div className="mb-8 flex flex-col items-center justify-between space-y-2 md:mb-10 md:flex-row lg:mb-12">
+            <h1 className="text-3xl font-bold tracking-tight text-foreground md:text-4xl">
               Meus Projetos
             </h1>
-            <div className="flex items-center gap-4">
-              <Input
-                type="text"
-                placeholder="Procurar projetos..."
-                className="rounded-md bg-background px-4 py-2 text-sm text-foreground placeholder:text-foreground"
-              />
-              <Select>
-                <SelectTrigger className="rounded-md bg-background px-4 py-2 text-sm text-foreground">
-                  <SelectValue placeholder="Filtrar por categoria" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="all" className="cursor-pointer">
-                    Todos
-                  </SelectItem>
-                  <SelectItem value="web" className="cursor-pointer">
-                    Web
-                  </SelectItem>
-                  <SelectItem value="mobile" className="cursor-pointer">
-                    Mobile
-                  </SelectItem>
-                  <SelectItem value="design" className="cursor-pointer">
-                    Design
-                  </SelectItem>
-                </SelectContent>
-              </Select>
+            <div className="flex w-full gap-4 md:w-auto">
+              <div className="mx-auto flex w-full max-w-xl items-center gap-2">
+                <div className="w-2/4 flex-1">
+                  <Input
+                    type="text"
+                    placeholder="Procurar projetos..."
+                    className="truncate rounded-md bg-background px-4 py-2 text-sm text-foreground placeholder:text-foreground"
+                  />
+                </div>
+                <div className="w-2/4 flex-1">
+                  <Select>
+                    <SelectTrigger className="truncate rounded-md bg-background px-4 py-2 text-sm text-foreground">
+                      <SelectValue placeholder="Filtrar por categoria" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="all" className="cursor-pointer">
+                        Todos
+                      </SelectItem>
+                      <SelectItem value="web" className="cursor-pointer">
+                        Web
+                      </SelectItem>
+                      <SelectItem value="mobile" className="cursor-pointer">
+                        Mobile
+                      </SelectItem>
+                      <SelectItem value="design" className="cursor-pointer">
+                        Design
+                      </SelectItem>
+                    </SelectContent>
+                  </Select>
+                </div>
+              </div>
             </div>
           </div>
           <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 md:gap-8 lg:grid-cols-4 lg:gap-10">
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -37,7 +37,7 @@ export default function Navbar() {
   console.log(pathname);
 
   return (
-    <header className="fixed left-0 top-0 z-50 w-full border-b bg-background">
+    <header className="fixed left-0 top-0 z-50 w-full border-b bg-background shadow-lg">
       <div className="mx-auto flex max-w-6xl items-center justify-between px-4 py-3 md:px-6">
         <Link
           href="/"
