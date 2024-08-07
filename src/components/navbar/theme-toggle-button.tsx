6e7fb63671680d21e93c89295ad7170e943b15e6
vtAndreola <dev.andreola@gmail.com>
2024-08-07 18:06:46 -0300

    style: remove dropdown menu from theme toggle button

--- src/components/navbar/theme-toggle-button.tsx
+++ src/components/navbar/theme-toggle-button.tsx
@@ -5,39 +5,21 @@ import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 
 export function ThemeToggle() {
-  const { setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
+    <div>
+      {theme === "light" ? (
+        <Button onClick={() => setTheme("dark")} variant="outline" size="icon">
+          <Moon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-0 dark:scale-0" />
         </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem
-          className="cursor-pointer hover:bg-zinc-100"
-          onClick={() => setTheme("light")}
-        >
-          Claro
-        </DropdownMenuItem>
-        <DropdownMenuItem
-          className="cursor-pointer hover:bg-zinc-100"
-          onClick={() => setTheme("dark")}
-        >
-          Escuro
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+      ) : (
+        <Button onClick={() => setTheme("light")} variant="outline" size="icon">
+          <Sun className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-90 dark:scale-100" />
+        </Button>
+      )}
+    </div>
   );
 }
