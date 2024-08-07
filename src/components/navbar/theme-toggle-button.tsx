ff1b5b2f09efee77e28771cf8f24b97278edbcaa
vtAndreola <dev.andreola@gmail.com>
2024-08-07 18:23:03 -0300

    fix: navbar details

--- src/components/navbar/theme-toggle-button.tsx
+++ src/components/navbar/theme-toggle-button.tsx
@@ -17,7 +17,7 @@ export function ThemeToggle() {
         </Button>
       ) : (
         <Button onClick={() => setTheme("light")} variant="outline" size="icon">
-          <Sun className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-90 dark:scale-100" />
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-90 dark:scale-100" />
         </Button>
       )}
     </div>
