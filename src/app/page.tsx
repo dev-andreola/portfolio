8d512b0d01e0a8c946689ff7b995ced7a54f052a
Vitor Andreola <dev.andreola@gmail.com>
2025-04-03 00:48:14 -0300

    feat: create tech stack section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import { Button } from "@/components/ui/button";
+import { InfiniteSlider } from "@/components/ui/infinite-slider";
 import { FolderIcon, MailIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import StackIcon from "tech-stack-icons";
 
 export default function Home() {
   return (
@@ -56,8 +58,48 @@ export default function Home() {
 
       <section id="about" className="w-full border-b bg-background">
         <div className="mx-auto max-w-6xl py-12 md:py-24">
-          <div className="container grid gap-8 px-4 md:grid-cols-2 md:gap-16 md:px-6">
-            <h1>opa</h1>
+          <div className="container">
+            <h2 className="mb-4 text-center text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
+              Tech Stack
+            </h2>
+            <p className="mb-16 text-center text-muted-foreground md:text-xl">
+              Tecnologias e ferramentas que domino e utilizo no meu dia a dia
+            </p>
+            <InfiniteSlider speedOnHover={20} gap={24}>
+              <StackIcon name="postgresql" grayscale className="size-14" />
+              <StackIcon name="reactjs" grayscale className="size-14" />
+              <StackIcon name="git" grayscale className="size-14" />
+              <StackIcon name="vscode" grayscale className="size-14" />
+              <StackIcon name="nextjs2" grayscale className="size-14" />
+              <StackIcon name="bash" grayscale className="size-14" />
+              <StackIcon name="bootstrap5" grayscale className="size-14" />
+              <StackIcon name="typescript" grayscale className="size-14" />
+              <StackIcon name="cloudinary" grayscale className="size-14" />
+              <StackIcon name="sass" grayscale className="size-14" />
+              <StackIcon name="ae" grayscale className="size-14" />
+              <StackIcon name="figma" grayscale className="size-14" />
+              <StackIcon name="python" grayscale className="size-14" />
+              <StackIcon name="firebase" grayscale className="size-14" />
+              <StackIcon name="graphql" grayscale className="size-14" />
+              <StackIcon name="docker" grayscale className="size-14" />
+              <StackIcon name="html5" grayscale className="size-14" />
+              <StackIcon name="aws" grayscale className="size-14" />
+              <StackIcon name="cypress" grayscale className="size-14" />
+              <StackIcon name="tailwindcss" grayscale className="size-14" />
+              <StackIcon name="github" grayscale className="size-14" />
+              <StackIcon name="analytics" grayscale className="size-14" />
+              <StackIcon name="ps" grayscale className="size-14" />
+              <StackIcon name="mongodb" grayscale className="size-14" />
+              <StackIcon name="nodejs" grayscale className="size-14" />
+              <StackIcon name="css3" grayscale className="size-14" />
+              <StackIcon name="mysql" grayscale className="size-14" />
+              <StackIcon name="postman" grayscale className="size-14" />
+              <StackIcon name="vitejs" grayscale className="size-14" />
+              <StackIcon name="ai" grayscale className="size-14" />
+              <StackIcon name="js" grayscale className="size-14" />
+              <StackIcon name="prisma" grayscale className="size-14" />
+              <StackIcon name="zod" grayscale className="size-14" />
+            </InfiniteSlider>
           </div>
         </div>
       </section>
@@ -82,18 +124,19 @@ export default function Home() {
               <div className="grid gap-1">
                 <h3 className="text-lg font-bold">Experiência</h3>
                 <p className="text-muted-foreground">
-                  Experiência como estagiário em desenvolvimento web utilizando
-                  typescript na empresa{" "}
+                  • Experiência como estagiário em desenvolvimento web
+                  utilizando typescript na empresa{" "}
                   <Link
                     href="https://www.appmasters.io/"
                     target="_blank"
-                    className="text-primary"
+                    className="text-primary hover:underline"
                   >
                     App Masters
-                  </Link>{" "}
+                  </Link>
+                  .
                 </p>
                 <p className="text-muted-foreground">
-                  Desenvolvimento de aplicações web freelancer para clientes e
+                  • Desenvolvimento de aplicações web freelancer para clientes e
                   profissionais autônomos.
                 </p>
               </div>
@@ -104,7 +147,7 @@ export default function Home() {
                   <Link
                     href="https://www.fiap.com.br/"
                     target="_blank"
-                    className="text-primary"
+                    className="text-primary hover:underline"
                   >
                     FIAP
                   </Link>
